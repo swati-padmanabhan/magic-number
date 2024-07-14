0cdcb170e4152546b328c3bc5d4d69bbdce26140
Swati P <67189695+swati-padmanabhan@users.noreply.github.com>
2024-07-14 18:53:30 +0530

    initial commit of magic number code

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value for radius: ");
+            int radius = Convert.ToInt32(Console.ReadLine());
+            double areaOfCircle = 3.14 * radius * radius; 
+            // here the value of "pi" that is 3.14 is a magic number because it has been hard coded
+            // instead, we should have declared a const and assign 3.14 to the variable "pi"
+            Console.WriteLine("Area of Circle is: " + areaOfCircle);
+
+        }
+    }
+}
